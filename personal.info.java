26425a06c5fbb7f80ef2bd4abbf977e52a6a6eb6
sabbaheugene460-ai <sabbaheugene460@gmail.com>
2026-06-10 15:50:02 +0000

    Add files via upload

---
+++ personal.info.java
@@ -0,0 +1,45 @@
+public class personalinfo{
+     public static void main(String[]arg){
+
+      String fullName = "Sabbah Eugene";
+
+           int age = 20;
+
+             char gender = 'F';
+
+               double hight = 1.65;
+
+                  float weight = 58.5f;
+
+                    boolean isStudent = true;
+
+                       long phonenenumber = 0594964485;
+
+                        String gradeLevel = "200Level";
+
+                         string program = "Computer Science";
+
+System .out.printIn("=====PERSONAL INFORMATION=====");
+
+     System.out.printIn("FULL NAME:" + fullname);
+
+System.out.print("AGE:" + age + " years");
+
+       System.out.printIn("GENDER:" + gender);
+
+System.out.printIn("HIGHT:" + hight + " m ");
+
+       System.out.printIn("WEIGHT:" + weight + " kg ");
+
+System.out.printIn("STUDENT:" + isStudent);
+
+          System.out.printIn("PHONE NUMBER:" + " + phone number);
+
+System.out.printIn("GRADE/LEVEL:" + gradelevel);
+
+           System.out.printIn("PROGRAM:" + program);
+
+System.out.printIn("==============================");
+
+}
+}
